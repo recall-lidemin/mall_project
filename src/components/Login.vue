275e6d813ebdfb91423353b2dc570c1b6971135f
lidemin-recall <1425889927@qq.com>
2020-02-24 00:35:03 +0800

    登陆表单页面绘制

---
+++ src/components/Login.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <div class="avatar_box">
+        <img src="../assets/logo.png" alt="" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less">
+.login_container {
+  height: 100%;
+  background-color: #2b4b6b;
+}
+
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 3px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  .avatar_box {
+    width: 130px;
+    height: 130px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    padding: 10px;
+    box-shadow: 0 0 10px #eee;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      background-color: #eee;
+    }
+  }
+}
+</style>
